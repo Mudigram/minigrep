aa77279669a45d83d721a490acedc5672d9e1c57
Mudiaga Temitayo Omene <omene.mudiaga@gmail.com>
2025-11-09 12:33:54 +0100

    Building A CLI Search tool in Rust

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        } else {
+            println!("Word not in line");
+        }
+    }
+
+    results
+}
